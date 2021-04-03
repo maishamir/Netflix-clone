0898a533d70265dbae363ac2ddea3e3f61f409a8
Maisha <maisha.mir2014@gmail.com>
2021-04-03 03:19:12 -0700

    files added

---
+++ netflix-clone/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Row from "./Row";
+import requests from "./requests";
+import Banner from "./Banner";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Navbar  */}
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchURL={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchURL={requests.fetchTrending} />
+      <Row title="Top Rated" fetchURL={requests.fetchTopRated} />
+      <Row title="Action" fetchURL={requests.fetchActionMovies} />
+      <Row title="Comedy" fetchURL={requests.fetchComedyMovies} />
+      <Row title="Horror" fetchURL={requests.fetchHorrorMovies} />
+      <Row title="Romance" fetchURL={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchURL={requests.fetchDocumentaries} />
+    </div>
+  );
+}
+
+export default App;
---
+++ netflix-clone/src/Row.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import requests from "./requests";
+import "./Row.css";
+
+const base_URL = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchURL, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+
+  // A snippet of code which runs based on a specific condition/variable
+  useEffect(() => {
+    // if [], run once when row loads, and don't run again
+    async function fetchData() {
+      const request = await axios.get(fetchURL);
+      setMovies(request.data.results);
+    }
+    fetchData();
+  }, [fetchURL]);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_URL}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Row;
